e4b27fd9f2618bd86e3ca192219da68dceb33746
nishina555 <n.toshi.1.014@gmail.com>
2025-02-05 11:38:43 +0900

    feat: 数値操作ユーティリティクラスの追加
    - 数値の偶数判定機能を追加\n- 素数判定機能を追加\n- 範囲内の数値合計計算機能を追加\n- 各機能のテストを追加

--- lib/number_utils.rb
+++ lib/number_utils.rb
@@ -1,25 +1,39 @@
 # frozen_string_literal: true
 
-class NumberUtils
-  # 数値が素数かどうかを判定する
-  def self.prime?(number)
-    return false if number <= 1
-    return true if number == 2
-
-    (2..Math.sqrt(number).to_i).each do |i|
-      return false if (number % i).zero?
+module NumberUtils
+  class << self
+    # 数値が偶数かどうかを判定する
+    # @param number [Integer] 判定対象の数値
+    # @return [Boolean] 偶数の場合はtrue、奇数の場合はfalse
+    def even?(number)
+      number % 2 == 0
     end
 
-    true
-  end
+    # 数値が素数かどうかを判定する
+    # @param number [Integer] 判定対象の数値
+    # @return [Boolean] 素数の場合はtrue、それ以外の場合はfalse
+    def prime?(number)
+      return false if number <= 1
+      return true if number == 2
 
-  # 指定された範囲内の素数を配列として返す
-  def self.find_primes(start, finish)
-    (start..finish).select { |num| prime?(num) }
-  end
+      (2..Math.sqrt(number).to_i).none? { |i| number % i == 0 }
+    end
 
-  # 数値の各桁の和を計算する
-  def self.sum_of_digits(number)
-    number.to_s.chars.map(&:to_i).sum
+    # 指定された範囲の数値の合計を計算する
+    # @param range [Range] 計算対象の範囲
+    # @return [Integer] 合計値
+    def sum_range(range)
+      range.sum
+    end
+
+    # 指定された範囲内の素数を配列として返す
+    def find_primes(start, finish)
+      (start..finish).select { |num| prime?(num) }
+    end
+
+    # 数値の各桁の和を計算する
+    def sum_of_digits(number)
+      number.to_s.chars.map(&:to_i).sum
+    end
   end
 end
